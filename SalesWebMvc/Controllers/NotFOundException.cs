750d8bd0dc37ac3755ad3dc32340a1f2e3679f3d
luiz.tozatti <luiz.tozatti@softplan.com.br>
2019-03-02 12:03:53 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Controllers/NotFOundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SalesWebMvc.Controllers
+{
+    [Serializable]
+    internal class NotFOundException : Exception
+    {
+        public NotFOundException()
+        {
+        }
+
+        public NotFOundException(string message) : base(message)
+        {
+        }
+
+        public NotFOundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotFOundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
